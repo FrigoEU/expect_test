8d26add12d12c41f1f0b160a985b3ba487b406ce
Simon Van Casteren <simon.van.casteren@gmail.com>
2021-12-06 20:18:36 +0100

    Don't use tsc

--- src/cli.ts
+++ src/cli.ts
@@ -4,25 +4,16 @@ import child_process from "child_process";
 import esbuild from "esbuild";
 import fs from "fs/promises";
 import path from "path";
-import prettier from "prettier";
-import ts, { CallExpression } from "typescript";
 
 const fileName = process.argv[2];
 
 // console.log(`Running expect-test on file: ${fileName}`);
 
 const fileNameParsed = path.parse(path.resolve(fileName));
-const whiteSpacePreservedFile = path.join(
-  fileNameParsed.dir,
-  "expect_test_temp" + fileNameParsed.ext
-);
 const outFilePath = path.join(fileNameParsed.dir, "out.js");
 
 async function cleanup() {
-  return fs
-    .unlink(whiteSpacePreservedFile)
-    .then(() => fs.unlink(outFilePath))
-    .then(() => fs.unlink(outFilePath + ".map"));
+  return fs.unlink(outFilePath).then(() => fs.unlink(outFilePath + ".map"));
 }
 
 try {
@@ -32,13 +23,8 @@ try {
 }
 
 async function go() {
-  await fs.writeFile(
-    whiteSpacePreservedFile,
-    preserveWhitespace(await fs.readFile(fileName, "utf-8"))
-  );
-
   const res = await esbuild.build({
-    entryPoints: [whiteSpacePreservedFile],
+    entryPoints: [fileName],
     bundle: true,
     sourcemap: true,
     platform: "node" as const,
@@ -61,8 +47,10 @@ async function go() {
   const stdout: string[] = [];
   const marker2Regex = /ExpectTestMarker2:([^:]*):(\d+):(\d+)/;
   proc.stdout.on("data", function (data: Buffer) {
+    debugger;
     data
       .toString()
+      .trimEnd()
       .split("\n")
       .forEach((str) => stdout.push(str));
     // console.log("stdout: " + data.toString());
@@ -73,18 +61,15 @@ async function go() {
   });
 
   proc.on("exit", async function (code) {
-    const parsedPath = path.parse(whiteSpacePreservedFile);
-    const origSource = await fs.readFile(whiteSpacePreservedFile, "utf8");
-    const oldSourceFile = ts.createSourceFile(
-      parsedPath.name + parsedPath.ext,
-      origSource,
-      ts.ScriptTarget.Latest // langugeVersion
-    );
-    let currentSourceFile: null | ts.SourceFile = null;
-
     let previousMarkerTwoIndex = 0;
     let markerOneIndex = null;
     if (code === 0) {
+      const changesToDo: {
+        file: string;
+        lineNumber: number;
+        columnNumber: number;
+        actual: string;
+      }[] = [];
       for (let i = 0; i < stdout.length; i++) {
         const data = stdout[i];
         if (data === "ExpectTestMarker1") {
@@ -110,76 +95,46 @@ async function go() {
               .trim();
             if (
               actual !== expected &&
-              path.resolve(whiteSpacePreservedFile) ===
-                path.resolve(callLocation.file)
+              path.resolve(fileName) === path.resolve(callLocation.file)
             ) {
-              const positionOfFirstCharacterOfLineWeNeed =
-                ts.getPositionOfLineAndCharacter(
-                  oldSourceFile,
-                  callLocation.lineNumber - 1,
-                  0
-                ) - 1;
-              const positionOfLastCharacterOfLineWeNeed =
-                ts.getPositionOfLineAndCharacter(
-                  oldSourceFile,
-                  callLocation.lineNumber,
-                  0
-                ) + 1;
-              let found = false;
-
-              const res: ts.TransformationResult<ts.SourceFile> = ts.transform<ts.SourceFile>(
-                currentSourceFile || oldSourceFile,
-                [
-                  // TODO: our search for the actual call is not really very precise
-                  // It works well if you have the "expect" call on a seperate line
-                  // but breaks down if you don't
-                  (context) => {
-                    const visit: ts.Visitor = (node) => {
-                      debugger;
-                      if (
-                        positionOfFirstCharacterOfLineWeNeed <= node.pos &&
-                        node.pos <= positionOfLastCharacterOfLineWeNeed &&
-                        node.kind === ts.SyntaxKind.CallExpression
-                      ) {
-                        found = true;
-                        return context.factory.createCallExpression(
-                          (node as CallExpression).expression,
-                          // context.factory.createIdentifier("expect"),
-                          undefined,
-                          [context.factory.createStringLiteral(actual)]
-                        );
-                      }
-                      return ts.visitEachChild(node, visit, context);
-                    };
-
-                    return (node) => ts.visitNode(node, visit);
-                  },
-                ],
-                {}
-              );
-              if (!found) {
-                throw new Error(
-                  `Bug in expect_test: couldn't find argument to replace @ ${callLocation.lineNumber}:${callLocation.columnNumber}`
-                );
-              }
-              currentSourceFile = res.transformed[0];
+              const change = {
+                ...callLocation,
+                actual,
+              };
+              changesToDo.push(change);
             }
             previousMarkerTwoIndex = i + 1;
           }
         }
       }
 
-      if (currentSourceFile !== null) {
-        const printer = ts.createPrinter({
-          newLine: ts.NewLineKind.LineFeed,
-        });
+      if (changesToDo.length !== 0) {
+        const parsedPath = path.parse(fileName);
+        const origSource = await fs.readFile(fileName, "utf8");
 
-        const result = printer.printFile(currentSourceFile);
+        const result: string = changesToDo.reduceRight(function (acc, change) {
+          debugger;
+          const allLines = acc.split("\n");
+          const linesAtEnd = allLines.slice(change.lineNumber - 1);
+          const last = [linesAtEnd[0].substr(change.columnNumber - 1)]
+            .concat(linesAtEnd.slice(1))
+            .join("\n");
+          const replaced = last.replace(
+            /\(["'`].*["'`]\)/,
+            change.actual.includes("\n")
+              ? "(`" + change.actual + "`)"
+              : '("' + change.actual + '")'
+          );
+          return (
+            allLines.slice(0, change.lineNumber - 1).join("\n") +
+            "\n" +
+            linesAtEnd[0].substr(0, change.columnNumber - 1) +
+            replaced
+          );
+        }, origSource);
+
         const correctedFileName = path.resolve(fileName + ".corrected");
-        await fs.writeFile(
-          correctedFileName,
-          prettier.format(restoreWhitespace(result), { parser: "typescript" })
-        );
+        await fs.writeFile(correctedFileName, result);
 
         console.log(`Found diff, wrote ${correctedFileName}`);
       }
@@ -192,12 +147,4 @@ async function go() {
       process.exit(code || 0);
     }
   });
-}
-
-// https://github.com/microsoft/TypeScript/issues/843#issuecomment-625530359
-function preserveWhitespace(content: string) {
-  return content.replace(/\n\n/g, "\n/** THIS_IS_A_NEWLINE **/");
-}
-function restoreWhitespace(content: string) {
-  return content.replace(/\/\*\* THIS_IS_A_NEWLINE \*\*\//g, "\n");
 }
--- test/test.ts
+++ test/test.ts
@@ -4,7 +4,7 @@ const adder = (a: number, b: number) => a + b;
 
 registerExpectTest(function (expect) {
   console.log(adder(5, 6).toString());
-  expect("1123");
+  expect("113");
 });
 
 const a = "kaa";
@@ -16,47 +16,5 @@ export function something() {
 registerExpectTest(function (e) {
   console.log(adder(5, 7).toString());
   console.log(adder(5, 7).toString());
-  e("13333");
-});
-
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
-});
-
-registerExpectTest(function (expect) {
-  console.log(adder(5, 7).toString());
-  expect("13333");
+  e("");
 });
